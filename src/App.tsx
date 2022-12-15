585f50c6ec4207b051c87c173b08735adf260367
kulotsystems <kulotsystems@gmail.com>
2022-12-15 16:38:49 +0800

    Remove Vite logo.

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ function App() {
     return (
         <div className="App">
             <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo"/>
-                </a>
                 <a href="https://reactjs.org" target="_blank">
                     <img src={reactLogo} className="logo react" alt="React logo"/>
                 </a>
@@ -25,7 +22,7 @@ function App() {
                 </p>
             </div>
             <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
+                Click on the React logo to learn more
             </p>
         </div>
     )
